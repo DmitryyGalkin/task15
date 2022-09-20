900b62fb054e38764cc8d80c0f2c6c97da9c3528
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-09-20 21:21:09 +0300

    day of week

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number == 6 ^ number == 7)
+    Console.WriteLine("Да - это выходной день");
+else if (number < 1 ^ number > 7)
+    Console.WriteLine("Такого дня недели нет");
+else
+    Console.WriteLine("Нет - это не выходной день");
+
